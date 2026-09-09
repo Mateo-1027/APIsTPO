ca30666e92d4f7387d35edc674a84fa7a04aac96
pat0nsio <patricioameri@hotmail.com>
2026-09-08 21:26:36 -0300

    Dependencia OpenAPI

--- src/main/java/com/_3d/marketplace/controllers/config/SecurityConfig.java
+++ src/main/java/com/_3d/marketplace/controllers/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                                                 .requestMatchers("/api/v1/auth/**").permitAll()
                                                 .requestMatchers("/actuator/health", "/actuator/info").permitAll()
                                                 .requestMatchers("/error").permitAll()
+                                                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
 
                                                 .requestMatchers(HttpMethod.GET, "/products/mine")
                                                 .hasAnyRole("ADMIN", "VENDOR")
